50f373c59eaf5ab162966d412f3e9802ce451a34
amulya30 <31375292+amulya30@users.noreply.github.com>
2021-06-21 21:48:55 +0530

    Update bolt_conf.py

--- bolt_conf.py
+++ bolt_conf.py
@@ -1,7 +1,7 @@
 """Configurations for bitcoin_alert.py"""
 
-bolt_api_key = "1f9ef2b3-e548-4d0b-915e-e947c869d58b"   
-device_id = "BOLT14885565"                    		
-telegram_chat_id = "@bit_amu"            
-telegram_bot_id = "bot1303272756:AAFm-QNH0qYe86Z9Q06BnQXlOOP0Rog1GNo"
+bolt_api_key = "xxxxx"   
+device_id = "BOLTxxxxx"                    		
+telegram_chat_id = "@xxxx"            
+telegram_bot_id = "xxxx"
       
